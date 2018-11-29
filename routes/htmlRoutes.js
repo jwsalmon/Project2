821fd52f1183c44fcc13db040cf4b03dc8316382
Jesse W. Salmon <j_aw_s@icloud.com>
2018-11-28 18:10:13 -0700

    intro page up

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -190,13 +190,19 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index"));
+  /* app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.handlebars"));
   });
-
+  app.get("/", function(req, res) {
+    res.render( "index");
+  });*/
+  app.get("/", function(req, res) {
+   // res.sendFile(path.join(__dirname, "index"));
+    res.render("index");
+  }); 
 
   app.get("/buyer", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/layouts/buyer"));
+    res.sendFile(path.join(__dirname, "/views/layouts/buyer"));
   });
 
  
